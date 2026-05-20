7528e19d2509470164e3d266929e3d6d300d2d70
rbits0 <rbits0@gmail.com>
2026-05-20 13:28:39 +1000

    Move game logic to GameUiState data class

--- app/src/main/java/com/rbits/devilition/ui/GameUiState.kt
+++ app/src/main/java/com/rbits/devilition/ui/GameUiState.kt
@@ -1,9 +1,16 @@
 package com.rbits.devilition.ui
 
+import android.util.Log
 import com.rbits.devilition.data.GRID_HEIGHT
 import com.rbits.devilition.data.GRID_WIDTH
 import com.rbits.devilition.data.HAND_SIZE
+import com.rbits.devilition.data.demonTypeHealth
+import com.rbits.devilition.data.demonsPerRound
+import com.rbits.devilition.data.piecesPerRound
+import kotlin.collections.component1
+import kotlin.collections.component2
 
+private const val TAG = "GameUiState"
 
 enum class DemonType {
     MINOR,
@@ -93,4 +100,116 @@ data class GameUiState(
         result = 31 * result + grid.contentDeepHashCode()
         return result
     }
+
+    fun roundStart(): GameUiState {
+        val grid = this.grid.map{ it.clone() }.toTypedArray()
+        val round = this.round + 1
+
+        if (round < 10) {
+            // Heal all elder demons
+            grid.forEachIndexed { rowIndex, row ->
+                row.forEachIndexed { colIndex, item ->
+                    if (item is GridItem.Demon && item.type == DemonType.ELDER) {
+                        grid[rowIndex][colIndex] = item.copy(health = item.maxHealth)
+                    }
+                }
+            }
+
+            val emptySpaces = getEmptySpaces(grid).toMutableSet()
+
+            // Place hole
+            val holePos = emptySpaces.random()
+            emptySpaces.remove(holePos)
+            grid[holePos.first][holePos.second] = GridItem.Hole()
+
+            // Place demons
+            demonsPerRound(round).forEach { (demonType, count) ->
+                val health = demonTypeHealth(demonType)
+
+                // Place `count` demons of `demonType`
+                for (i in 0..<count) {
+                    val demonPos = emptySpaces.random()
+                    emptySpaces.remove(demonPos)
+                    grid[demonPos.first][demonPos.second] = GridItem.Demon(
+                        type = demonType,
+                        health = health,
+                        maxHealth = health,
+                    )
+                }
+            }
+        } else {
+            // FINAL ROUND
+
+            // Replace all demons with holes
+            grid.forEachIndexed { rowIndex, row ->
+                row.forEachIndexed { colIndex, item ->
+                    if (item is GridItem.Demon) {
+                        grid[rowIndex][colIndex] = GridItem.Hole()
+                    }
+                }
+            }
+
+            // Replace 4x4 square in middle with holes
+            // All spots from (3, 3) to (6, 6)
+            for (rowIndex in 3..6) {
+                for (colIndex in 3..6) {
+                    grid[rowIndex][colIndex] = GridItem.Hole()
+                }
+            }
+
+            // Place boss in middle
+            // Boss takes up a 2x2 space
+            grid[4][4] = GridItem.Demon(
+                type = DemonType.BOSS,
+                health = demonTypeHealth(DemonType.BOSS),
+                maxHealth = demonTypeHealth(DemonType.BOSS),
+            )
+            for (pos in listOf(Pair(4, 5), Pair(5, 4), Pair(5, 5))) {
+                grid[pos.first][pos.second] = GridItem.BossHitbox(
+                    bossPos = Pair(4, 4),
+                )
+            }
+
+            // Place 2-4 holes
+            // TODO: Check the specifics of how this is done in UFO 50
+            val emptySpaces = getEmptySpaces(grid).toMutableSet()
+            val numHoles = (2..4).random()
+            for (i in 0..<numHoles) {
+                val holePos = emptySpaces.random()
+                emptySpaces.remove(holePos)
+                grid[holePos.first][holePos.second] = GridItem.Hole()
+            }
+        }
+
+        val numAvailablePieces = this.numAvailablePieces + piecesPerRound(round)
+
+        return this.copy(
+            grid = grid,
+            round = round,
+            numAvailablePieces = numAvailablePieces,
+        )
+    }
+
+    fun movePiece(item: GridItem.Piece, to: Pair<Int, Int>): GameUiState {
+        val grid = this.grid.map { it.clone() }.toTypedArray()
+
+        val from = item.position
+        if (from == null) {
+            Log.e(TAG, "Piece position is null")
+            return this
+        }
+        grid[from.first][from.second] = null
+        grid[to.first][to.second] = item.copy(position = to)
+
+        return this.copy(grid = grid)
+    }
 }
+
+fun getEmptySpaces(grid: Array<Array<GridItem?>>): List<Pair<Int, Int>> {
+    return grid.flatMapIndexed { rowIndex, row ->
+        row.mapIndexedNotNull { colIndex, item ->
+            if (item == null) Pair(rowIndex, colIndex) else null
+        }
+    }
+}
+
--- app/src/main/java/com/rbits/devilition/ui/GameViewModel.kt
+++ app/src/main/java/com/rbits/devilition/ui/GameViewModel.kt
@@ -1,133 +1,19 @@
 package com.rbits.devilition.ui
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.rbits.devilition.data.demonTypeHealth
-import com.rbits.devilition.data.demonsPerRound
-import com.rbits.devilition.data.piecesPerRound
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlin.collections.mapOf
-
-fun getEmptySpaces(grid: Array<Array<GridItem?>>): List<Pair<Int, Int>> {
-    return grid.flatMapIndexed { rowIndex, row ->
-        row.mapIndexedNotNull { colIndex, item ->
-            if (item == null) Pair(rowIndex, colIndex) else null
-        }
-    }
-}
 
 class GameViewModel : ViewModel() {
-    private val tag = "GameViewModel"
-
     private val _uiState = MutableStateFlow(GameUiState())
     val uiState = _uiState.asStateFlow()
 
     fun roundStart() {
-        _uiState.update { currentState ->
-            val grid = currentState.grid.map{ it.clone() }.toTypedArray()
-            val round = currentState.round + 1
-
-            if (round < 10) {
-                // Heal all elder demons
-                grid.forEachIndexed { rowIndex, row ->
-                    row.forEachIndexed { colIndex, item ->
-                        if (item is GridItem.Demon && item.type == DemonType.ELDER) {
-                            grid[rowIndex][colIndex] = item.copy(health = item.maxHealth)
-                        }
-                    }
-                }
-
-                val emptySpaces = getEmptySpaces(grid).toMutableSet()
-
-                // Place hole
-                val holePos = emptySpaces.random()
-                emptySpaces.remove(holePos)
-                grid[holePos.first][holePos.second] = GridItem.Hole()
-
-                // Place demons
-                demonsPerRound(round).forEach { (demonType, count) ->
-                    val health = demonTypeHealth(demonType)
-
-                    // Place `count` demons of `demonType`
-                    for (i in 0..<count) {
-                        val demonPos = emptySpaces.random()
-                        emptySpaces.remove(demonPos)
-                        grid[demonPos.first][demonPos.second] = GridItem.Demon(
-                            type = demonType,
-                            health = health,
-                            maxHealth = health,
-                        )
-                    }
-                }
-            } else {
-                // FINAL ROUND
-
-                // Replace all demons with holes
-                grid.forEachIndexed { rowIndex, row ->
-                    row.forEachIndexed { colIndex, item ->
-                        if (item is GridItem.Demon) {
-                            grid[rowIndex][colIndex] = GridItem.Hole()
-                        }
-                    }
-                }
-
-                // Replace 4x4 square in middle with holes
-                // All spots from (3, 3) to (6, 6)
-                for (rowIndex in 3..6) {
-                    for (colIndex in 3..6) {
-                        grid[rowIndex][colIndex] = GridItem.Hole()
-                    }
-                }
-
-                // Place boss in middle
-                // Boss takes up a 2x2 space
-                grid[4][4] = GridItem.Demon(
-                    type = DemonType.BOSS,
-                    health = demonTypeHealth(DemonType.BOSS),
-                    maxHealth = demonTypeHealth(DemonType.BOSS),
-                )
-                for (pos in listOf(Pair(4, 5), Pair(5, 4), Pair(5, 5))) {
-                    grid[pos.first][pos.second] = GridItem.BossHitbox(
-                        bossPos = Pair(4, 4),
-                    )
-                }
-
-                // Place 2-4 holes
-                // TODO: Check the specifics of how this is done in UFO 50
-                val emptySpaces = getEmptySpaces(grid).toMutableSet()
-                val numHoles = (2..4).random()
-                for (i in 0..<numHoles) {
-                    val holePos = emptySpaces.random()
-                    emptySpaces.remove(holePos)
-                    grid[holePos.first][holePos.second] = GridItem.Hole()
-                }
-            }
-
-            val numAvailablePieces = currentState.numAvailablePieces + piecesPerRound(round)
-
-            currentState.copy(
-                grid = grid,
-                round = round,
-                numAvailablePieces = numAvailablePieces,
-            )
-        }
+        _uiState.update { it.roundStart() }
     }
 
     fun movePiece(item: GridItem.Piece, to: Pair<Int, Int>) {
-        _uiState.update { currentState ->
-            val grid = currentState.grid.map{ it.clone() }.toTypedArray()
-
-            val from = item.position
-            if (from == null) {
-                Log.e(tag, "Piece position is null")
-                return
-            }
-            grid[from.first][from.second] = null
-            grid[to.first][to.second] = item.copy(position = to)
-
-            currentState.copy(grid = grid)
-        }
+        _uiState.update { it.movePiece(item, to) }
     }
 }

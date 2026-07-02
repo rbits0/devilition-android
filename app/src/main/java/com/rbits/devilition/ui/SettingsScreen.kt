568dd19ad7a8bf2bef0c0389bd774c6fe47f49c4
rbits0 <rbits0@gmail.com>
2026-07-02 17:13:40 +1000

    Use stringResource for settings text

--- app/src/main/java/com/rbits/devilition/ui/SettingsScreen.kt
+++ app/src/main/java/com/rbits/devilition/ui/SettingsScreen.kt
@@ -10,9 +10,11 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rbits.devilition.LIST_SCREEN_PADDING_DP
+import com.rbits.devilition.R
 import com.rbits.devilition.data.Settings
 import com.rbits.devilition.ui.theme.DevilitionTheme
 
@@ -44,7 +46,7 @@ fun SettingsScreen(
                     )
                 }
             ) {
-                Text("Enable time bonus")
+                Text(stringResource(R.string.enable_time_bonus))
             }
         }
     }
